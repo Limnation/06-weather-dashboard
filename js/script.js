225b8c30426b7fc610d7b7ade1b283da9c873fed
Scott Erwin <scottallenerwin@gmail.com>
2021-03-21 21:58:09 -0400

    nested another api because i needed to used the lat and long from the first api to get the same location to get the UV from the city

--- js/script.js
+++ js/script.js
@@ -1,35 +1,58 @@
-var fetchButton = document.getElementById('button-addon2');
+var button = $('#button-addon2');
 var input = $('#search').val();
-
+// C for content in html
+var cityC = $('#cityC');
+var tempC = $('#tempC');
+var humidityC = $('#humidityC');
+var windSpeedC = $('#windSpeedC');
+var UVindexC = $('#UVindexC');
+          
 function getApi() {
 
     var input = $('#search').val();
     var cityName = encodeURIComponent(input);
+    // added imperial to the api to get units in Fahrenheit
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=dfbc98f0dc3dbb36a4bc6a2139fe1754`
 
-    var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=dfbc98f0dc3dbb36a4bc6a2139fe1754`
-    
     fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        //Using console.log to examine the data
-        console.log(data);
-        // for (var i = 0; i < data.length; i++) {
-        //   //Creating a h3 element and a p element
-        //   var userName = document.createElement('h3');
-        //   var userUrl = document.createElement('p');
-  
-        //   //Setting the text of the h3 element and p element.
-        //   userName.textContent = data[i].login;
-        //   userUrl.textContent = data[i].url;
-  
-        //   //Appending the dynamically generated html to the div associated with the id="users"
-        //   //Append will attach the element as the bottom most child.
-        //   usersContainer.append(userName);
-        //   usersContainer.append(userUrl);
-        // }
-      });
-  }
-  fetchButton.addEventListener('click', getApi);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //Using console.log to examine the data
+            console.log(data);
+            // //these r the V for value/output
+            var cityV = data.name;
+            cityC.append(cityV);
+                
+            var tempV = data.main.temp;
+            tempC.append(tempV);
+                
+            var humidityV= data.main.humidity;
+            humidityC.append(humidityV);
+                
+            var windSpeedV= data.wind.speed;
+            windSpeedC.append(windSpeedV);
+
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+
+            var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=dfbc98f0dc3dbb36a4bc6a2139fe1754`
+    
+            fetch(url)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (dataOne) {
+                    //Using console.log to examine the data
+                    console.log(dataOne);
+
+                    var UVindexV= dataOne.current.uvi;
+                    UVindexC.append(UVindexV);
+                });
+        });
+    
+    
+}
+button.on('click', getApi);
   
