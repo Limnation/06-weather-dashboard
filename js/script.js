32d224a4a30f43d66d65c94d99c21e753b77c344
Scott Erwin <scottallenerwin@gmail.com>
2021-03-21 20:15:13 -0400

    got API url to work and fetch info and console.log it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-var url = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=dfbc98f0dc3dbb36a4bc6a2139fe1754`
+var fetchButton = document.getElementById('button-addon2');
+var input = $('#search').val();
 
-var cityName = encodeURIComponent($('search').val());
+function getApi() {
+
+    var input = $('#search').val();
+    var cityName = encodeURIComponent(input);
+
+    var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=dfbc98f0dc3dbb36a4bc6a2139fe1754`
+    
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        //Using console.log to examine the data
+        console.log(data);
+        // for (var i = 0; i < data.length; i++) {
+        //   //Creating a h3 element and a p element
+        //   var userName = document.createElement('h3');
+        //   var userUrl = document.createElement('p');
+  
+        //   //Setting the text of the h3 element and p element.
+        //   userName.textContent = data[i].login;
+        //   userUrl.textContent = data[i].url;
+  
+        //   //Appending the dynamically generated html to the div associated with the id="users"
+        //   //Append will attach the element as the bottom most child.
+        //   usersContainer.append(userName);
+        //   usersContainer.append(userUrl);
+        // }
+      });
+  }
+  fetchButton.addEventListener('click', getApi);
+  
